4c9f847d1fe055fe6d89ad6185d29535545805ea
Anton Medbo <medbo95@gmail.com>
2022-07-21 11:57:30 +0200

    test

--- src/Components/Products/ProductList.js
+++ src/Components/Products/ProductList.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import ProductRow from './ProductRow';
+import ProductData from './ProductData';
+
+
 
 class ProductList extends Component {
   constructor(props) {
     super(props);
   }
-
   
 
   render() {
     return (
       <div className="container main-content">
         {/* for each loop */}
+        <ProductRow items={ProductData[1]}/>
+        {/* <ProductRow />
         <ProductRow />
         <ProductRow />
         <ProductRow />
@@ -25,12 +29,11 @@ class ProductList extends Component {
         <ProductRow />
         <ProductRow />
         <ProductRow />
-        <ProductRow />
-        <ProductRow />
-        <ProductRow />
+        <ProductRow /> */}
       </div>
     );
   }
 }
 
 export default ProductList;
+
